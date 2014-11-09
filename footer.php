36c433fbbf96ecd3c08f0bf53894e29d3a33f3d7
RandyHandy <localhost@mail.com>
2014-11-09 01:30:04 +0100

    Mejoras en el footer

--- footer.php
+++ footer.php
@@ -4,13 +4,13 @@
 		</div>
 		<div id="footer">
 			<div class="footer-text">
-				Copyright © 2013 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> - Todos los derechos reservados. Diseño original por www.Vozidea.com.<br/>
+				Copyright © 2014 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> - Todos los derechos reservados. Diseño original por <a href="https://github.com/Zeokat/vozideatheme">Vozidea</a>.<br/>
 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> No Aloja Ningún Archivo En Sus Servidores, Todo Contenido De La Web Hace Referencia a Servidores Externos.
 			</div>
 		</div>
 	</div>
 </div>
 	<?php wp_footer(); ?>
-	<!-- Don't forget analytics -->
+	<!-- No olvides Google analytics -->
 </body>
 </html>
